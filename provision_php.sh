1f9609b414b9cf9016b5b34e6b3bbb8e425851f1
xigolle <joey.driessen@live.nl>
2016-05-22 16:06:20 +0200

    add silent install

--- provision_php.sh
+++ provision_php.sh
@@ -1,3 +1,3 @@
-echo "----------------installing PHP"
-apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+echo "----------------installing PHP----------------"
+apt-get -y install php5 libapache2-mod-php5 php5-mcrypt > /dev/null
 service apache2 restart
